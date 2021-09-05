16e3bc1b2a0f824bd6d3c2bf0945fc284552a01f
Shivani5078 <88040014+Shivani5078@users.noreply.github.com>
2021-09-06 02:14:11 +0530

    add:intal commite

---
+++ PrimeFactorNumber .java
@@ -0,0 +1,28 @@
+package company;
+
+import java.util.*;
+public class PrimeFactorNumber {
+	public static void main(String[] args) {
+	
+	// Get input from the user
+
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number");
+	int number=sc.nextInt();
+
+	// find prime factors of given number using for loop
+
+	for(int i = 2; i< number; i++)  {
+		while(number%i == 0) {
+       			System.out.println(i+" ");
+	        	number = number/i;
+  		}
+	}
+	if(number >2) {
+
+	System.out.println(number); 
+
+	}
+
+	}
+}
