a1a3d7bca56c83711036dd2719969eaad85644b7
Shivani5078 <88040014+Shivani5078@users.noreply.github.com>
2021-09-06 01:24:28 +0530

    Create maximumString.java

---
+++ maximumString.java
@@ -0,0 +1,70 @@
+package company;
+	public void maximumString() {
+	
+	ArrayList<String> stringvalue = new ArrayList<String>();
+	
+    stringvalue.add("papaya");
+    stringvalue.add("banana");
+    stringvalue.add("orange");
+    stringvalue.add("mango");
+    stringvalue.add("apple");
+    
+    Collections.sort(stringvalue, Collections.reverseOrder());
+    
+    
+	System.out.println("The Maximum string value is:" + stringvalue);
+    
+	}
+    
+    public static void main(String args []) {
+    	UC4_MoreThanThreeParameter obj = new UC4_MoreThanThreeParameter();
+    	obj.maximumString();
+    	
+    }
+
+}
+	ArrayList<String> stringvalue = new ArrayList<String>();//arraylist
+	
+    stringvalue.add("papaya");
+    stringvalue.add("banana");
+    stringvalue.add("orange");
+    stringvalue.add("mango");
+    stringvalue.add("apple");
+    
+    Collections.sort(stringvalue, Collections.reverseOrder());
+    
+    
+	System.out.println("The Maximum string value is:" + stringvalue);
+    
+	}
+    
+    public static void main(String args []) {
+    	UC4_MoreThanThreeParameter obj = new UC4_MoreThanThreeParameter();
+    	obj.maximumString();
+    	
+    }
+
+}
+	
+	ArrayList<String> stringvalue = new ArrayList<String>();
+	
+    stringvalue.add("papaya");
+    stringvalue.add("banana");
+    stringvalue.add("orange");
+    stringvalue.add("mango");
+    stringvalue.add("apple");
+    
+    Collections.sort(stringvalue, Collections.reverseOrder());
+    
+    
+	System.out.println("The Maximum string value is:" + stringvalue);
+    
+	}
+    
+    public static void main(String args []) {
+    	UC4_MoreThanThreeParameter obj = new UC4_MoreThanThreeParameter();
+    	obj.maximumString();
+    	
+    }
+
+}
